96a90f3bd584d94fda84b48ae1fdc73304483e69
potaroo <enohpolyx9310@naver.com>
2024-01-14 01:50:22 +0900

    다이나믹 프로그래밍 연습문제 N

--- Week4_Dynamic_Programming/boj_30621.cpp
+++ Week4_Dynamic_Programming/boj_30621.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+// #define DEBUG
 using namespace std;
 constexpr int MX = 100'000;
 
@@ -33,17 +34,24 @@ int main() {
     dp[1] = a[1].c;
     for(int i = 2; i <= n; ++i) {
         int lim = a[i].t - a[i].b; // lim 이후부터 발성하면 안됨
-        for(int left = 1, right = i, mid = (i + 1) / 2; left <= right; mid = (left + right) / 2) {
-            if(a[mid].t >= lim) { // lim 이후에 발성한 경우이므로 새롭게 발성할 수 없음
+        dp[i] = a[i].c;
+        for(int left = 0, right = i - 1, mid = (i - 1) / 2; left <= right; mid = (left + right) / 2) {
+            if(a[mid].t >= lim) { // lim 이후에 발성한 경우이므로 새롭게 발성할 수 없음, 더 작은 mid값 탐색
                 right = mid - 1;
             }
-            else { // 새롭게 발성할 수 있는 경우 해당하는 경우 중 dp[i]를 가장 크게 갱신할 수 있는 값을 찾아감
+            else { // 발성할 수 있는 경우 해당하는 경우 중 dp[i]를 가장 크게 갱신할 수 있는 값을 찾아감
                 left = mid + 1;
+                #ifdef DEBUG
+                    cout << "i : " << i << " lim : " << lim << " mid : " << mid << " dp[mid] : " << dp[mid] << '\n';
+                #endif
                 if(dp[i] < dp[mid] + a[i].c)
                     dp[i] = dp[mid] + a[i].c;
             }
         }
         if(ans < dp[i]) ans = dp[i];
+        #ifdef DEBUG
+        cout << "i : " << i << ", dp[i] : " << dp[i] << '\n';
+        #endif
     }
     cout << ans << '\n';
 }
