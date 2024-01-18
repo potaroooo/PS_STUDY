a1a76641194a2215e02bf73ea72d6cd968fd2ec0
potaroo <enohpolyx9310@naver.com>
2024-01-18 17:29:01 +0900

    애드 혹 연습문제 H AC

---
+++ Week5/Ad_Hoc/boj_13018.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    cin.sync_with_stdio(0);
+
+    int n, k;
+    cin >> n >> k;
+    if(n == k) {
+        cout << "Impossible\n";
+        return 0;
+    }
+
+    int swap_cnt = n - k - 1, last_swap = 0;
+    int *a = new int[n + 2];
+    for(int i = 1; i <= n; ++i) {
+        a[i] = i;
+    }
+    for(int i = 1; i <= swap_cnt; ++i) {
+        swap(a[i], a[i + 1]);
+    }
+    for(int i = 1; i <= n; ++i)
+        cout << a[i] << ' ';
+}
