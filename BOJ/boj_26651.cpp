576cdca68027f017ff0f7bab64f184af5c8600d0
potaroo <enohpolyx9310@naver.com>
2024-02-25 18:20:09 +0900

    작업중인 소스 저장

---
+++ BOJ/boj_26651.cpp
@@ -0,0 +1,19 @@
+// 팬램그
+#include <iostream>
+#include <vector>
+using namespace std;
+constexpr int MX = 100'000;
+constexpr int MOD = 30'000;
+
+int X;
+vector<int> v;
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    cin.sync_with_stdio(0);
+    cin >> X;
+    for(int i = 1; i * i <= X; ++i) {
+        
+    }
+}
